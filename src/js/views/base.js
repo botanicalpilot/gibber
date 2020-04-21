2dc5ff26dd863eb4b962d34d259838420ad107ff
Akins <smo244@gmail.com>
2020-04-21 12:57:11 -0700

    added new loading spinner svg and got it running

--- src/js/views/base.js
+++ src/js/views/base.js
@@ -4,5 +4,24 @@ export const elements = {
     searchRes: document.querySelector('.results'),
     searchResList: document.querySelector('.results__list'),
     searchParam: document.querySelector('.search__options'),
-    
 }
+
+export const elementStrings = {
+    loader: 'loader'
+};
+
+export const renderLoader = parent => {
+    const loader = `
+        <div class="${elementStrings.loader}">
+            <svg>
+                <use href="img/icons.svg#icon-circles-cw"></use>
+            </svg>
+        </div>
+    `;
+    parent.insertAdjacentHTML('afterbegin', loader);
+};
+
+export const clearLoader = () => {
+    const loader = document.querySelector(`.${elementStrings.loader}`);
+    if(loader) loader.parentElement.removeChild(loader);
+};
