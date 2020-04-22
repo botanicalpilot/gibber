07ba863980c3580789a3117692b29d0006bcbcdc
Akins <smo244@gmail.com>
2020-04-22 13:59:42 -0700

    created png logo and began styling on page

--- src/js/models/Search.js
+++ src/js/models/Search.js
@@ -11,11 +11,12 @@ export default class Search {
             console.log(`query: ${query}`)
             const res = await axios.get(`${proxy}https://thallus-api.herokuapp.com/api/crops?common_name=${query}`);
             
+            // if res returns empty array, try again by searching with scientific name. 
             if(res.data.length < 1){
                 const res2 = await axios.get(`${proxy}https://thallus-api.herokuapp.com/api/crops?scientific_name=${query}`)
                 this.result = res2.data
             } else {this.result = res.data;}
-            
+
         }catch (error) {
             console.log(error)
             alert(error) 
