a03f252bc8b0b3c3aaab07fdb828748539eb5016
Akins <smo244@gmail.com>
2020-04-27 12:35:56 -0700

    updated searchView to display only dates in panel != null

--- src/js/views/searchView.js
+++ src/js/views/searchView.js
@@ -40,22 +40,69 @@ export const buildAccordion = () => {
     }     
 }
 
+export const splitDate = rawDate => {
+    return rawDate.replace('T00:00:00.000Z', '').split('-').reverse().join('-')
+}
+
+
 const renderCrop = crop => {
     // let users know planting intervals. 
     let cropBoolean = ''
     if(crop.sow_all_season && crop.start_all_season === true){
        cropBoolean =  `<div class="results_boolean">Plant Crop in intervals all season from either starts or seeds.</div>` 
-    } else if(crop.sow_all_season === true && crop.start_all_season === false){ cropBoolean = `<div class="results_boolean">Crops may be sown in intervals throughout the growing season.</div>`
+    } else if(crop.sow_all_season === true && crop.start_all_season === false){ cropBoolean = `<div class="results_boolean">Crop may be sown in intervals throughout the growing season.</div>`
     } else {
         cropBoolean = `<div class="results__boolean">Crops may only be planted between the dates below.</div>`
     }
 
-    // reformat dates from API
-    // console.log(new Date(crop.sow_outdoor_start))
+    // check if null/reformat dates for indoor planting dates 
+    let cropIndoorStart = ''
+    let cropIndoorEnd = ''
+    let indoorClass = ''
+    if(crop.sow_indoor_start && crop.sow_indoor_end != null){
+        cropIndoorStart = splitDate(crop.sow_indoor_start)
+        cropIndoorEnd = splitDate(crop.sow_indoor_end)
+        indoorClass = 'sow_indoor'
+    } else {
+        cropIndoorStart = crop.sow_indoor_start
+        cropIndoorEnd = crop.sow_indoor_end
+        indoorClass = 'sow_indoor_hide'
+    }
+
+    // check if null/reformat dates for sowing dates 
+    let cropSowStart = ''
+    let cropSowEnd = ''
+    let outdoorClass = ''
+    if(crop.sow_outdoor_start && crop.sow_outdoor_end != null){
+        cropSowStart = splitDate(crop.sow_outdoor_start)
+        cropSowEnd = splitDate(crop.sow_outdoor_end)
+        outdoorClass = 'sow_outdoor'
+    } else {
+        cropSowStart = crop.sow_outdoor_start
+        cropSowEnd = crop.sow_outdoor_end
+        outdoorClass = 'sow_outdoor_hide'
+    }
+
+    // check if null/reformat dates for start dates 
+    let startBegin = ''
+    let startEnd = ''
+    let startClass = ''
+    if(crop.start_outdoor_start && crop.start_outdoor_end != null){
+        startBegin = splitDate(crop.start_outdoor_start)
+        startEnd = splitDate(crop.start_outdoor_end)
+        startClass = 'start_outdoor'
+    } else {
+        startBegin = crop.start_outdoor_start
+        startEnd = crop.start_outdoor_end
+        startClass = 'start_outdoor_hide'
+    }
+
+
+    
 
     const markup = `
     <li>
-    <a class="results__link results__link--active" href="#23456">
+    <a class="results__link results__link--active">
         <button class="results__data">
             <div id="cn_style">${crop.common_name}</div><br>
             <div id="sn_style">${crop.scientific_name}</div>
@@ -63,19 +110,19 @@ const renderCrop = crop => {
         <div class="results__panel">
             ${cropBoolean} 
             <div class="results__dates">
-                <div class="sow_indoor">
-                    <p>Sow Indoor Start: ${crop.sow_indoor_start}</p>
-                    <p>end: Sow Indoor End: ${crop.sow_indoor_end}</p>
+                <div class=${indoorClass}>
+                    <p>Sow Indoor Start: ${cropIndoorStart}</p>
+                    <p>end: Sow Indoor End: ${cropIndoorEnd}</p>
                     <button>add</button>
                 </div>
-                <div class="sow_outdoor">
-                    <p>Sow Outdoor Start:<br> ${crop.sow_outdoor_start}</p>
-                    <p>Sow Outdoor End: ${crop.sow_outdoor_end}</p>
+                <div class="${outdoorClass}">
+                    <p>Sow Outdoor Start:<br> ${cropSowStart}</p>
+                    <p>Sow Outdoor End: ${cropSowEnd}</p>
                     <button>add</button>
                 </div>
-                <div class="start_outdoor">
-                    <p>Plant Starts Outdoor: ${crop.start_outdoor_start}</p>
-                    <p>Plant Starts Outdoor End: ${crop.start_outdoor_end}
+                <div class="${startClass}">
+                    <p>Plant Starts Outdoor: ${startBegin}</p>
+                    <p>Plant Starts Outdoor End: ${startEnd}
                 <button>add</button>
                 </div>
             </div>
